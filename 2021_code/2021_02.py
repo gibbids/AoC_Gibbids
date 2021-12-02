56a2758cd5cbd14c67ec8f47457d0e92c4cb6285
Gabe Osmo <48137808+gibbids@users.noreply.github.com>
2021-12-02 16:19:13 +1100

    day 2 completed

--- 2021_code/2021_02.py
+++ 2021_code/2021_02.py
@@ -1,7 +1,33 @@
 import aocd
 
-input_list = [int(n) for n in aocd.get_data(day=1, year=2021).splitlines()]
+input_list = aocd.get_data(day=2, year=2021).splitlines()
 
+depth = 0
+horizontal = 0
 
+for i in input_list:
+    i = i.split()
+    if i[0] == 'up':
+        depth -= int(i[1])
+    elif i[0] == 'down':
+        depth += int(i[1])
+    elif i[0] == 'forward':
+        horizontal += int(i[1])
 
-print(result)
+print(horizontal * depth)
+
+depth = 0
+horizontal = 0
+aim = 0
+
+for i in input_list:
+    i = i.split()
+    if i[0] == 'up':
+        aim -= int(i[1])
+    elif i[0] == 'down':
+        aim += int(i[1])
+    elif i[0] == 'forward':
+        horizontal += int(i[1])
+        depth += aim * int(i[1])
+
+print(horizontal * depth)
